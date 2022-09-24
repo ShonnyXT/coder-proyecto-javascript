aa95cc3f1598519ab6e4d4b521162ec502ac036d
Jonatan Eduardo Mamani <jonny.m722@gmail.com>
2022-09-24 20:44:13 -0300

    initial commit

---
+++ assets/javascript/main.js
@@ -0,0 +1,13 @@
+confirm ('¿Eres mayor de edad?')
+
+let edad1 = 18;
+let edad2 = parseInt ( prompt ('Ingrese su edad') )
+
+if (edad2 >= edad1) {
+    alert ('Bienvenidos a Perritos Cachondos Online')
+    
+}
+else {
+    alert ('Usted aún es un cachorro')
+}
+
