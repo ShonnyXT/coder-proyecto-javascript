acbba6becf8ba002a364b6c9d487999ae28e5cd1
Jonatan Eduardo Mamani <jonny.m722@gmail.com>
2022-09-26 18:07:21 -0300

    calculadoraDePrestamos

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,13 +1,24 @@
-confirm ('¿Eres mayor de edad?')
 
-let edad1 = 18;
-let edad2 = parseInt ( prompt ('Ingrese su edad') )
+let num = parseInt(prompt('Ingrese el monto del prestamo','100000'));
+let cuotas = parseInt(prompt('Ingrese cantidad de cuotas','12'))
+let mensaje;
 
-if (edad2 >= edad1) {
-    alert ('Bienvenidos a Perritos Cachondos Online')
-    
+const verificar = (numero) => {
+    num = numero
+    if ( !isNaN(num) ) {
+        if (num >= 10000 && num <= 100000) {
+            /* resultado final */
+            mensaje = alert (Math.floor((num * 2) / cuotas))
+        }else{
+            while (10000 > num < 100000) {
+                mensaje = alert ('El prestamo esta fuera de rango')
+                num = parseInt(prompt('Ingrese un monto entre 10.000 y 100.000','10000'))
+            }
+        }
+    }else{
+        mensaje = alert ('Debes ingresar solo numeros')
+        num = parseInt(prompt('Ingrese el monto de prestamo','100000'))
+        verificar (num)
+    }
 }
-else {
-    alert ('Usted aún es un cachorro')
-}
-
+verificar (num)
