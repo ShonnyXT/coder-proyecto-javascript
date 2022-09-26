5bb98f468168146086147f714d35ab6b44078424
Jonatan Eduardo Mamani <jonny.m722@gmail.com>
2022-09-26 19:42:09 -0300

    resubida

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,49 +1,6 @@
-/* confirm ('¿Eres mayor de edad?')
-
-let edad1 = 18;
-let edad2 = parseInt ( prompt ('Ingrese su edad') )
-
-if (edad2 >= edad1) {
-    alert ('Bienvenidos a Perritos Cachondos Online')
-    
-}
-else {
-    alert ('Usted aún es un cachorro')
-} */
-
-/* let robar = 1
-let quedarse = 2
-
-let jugada = parseInt (prompt ('Ingrese 1 para Robar o 2 para quedarse', '1'))
-
-// dealer barajador
-function random (min, max) {
-    return Math.floor((Math.random() * (max - min)) + min);
-}
-
-console.log (random ( 17 , 22 )) */
-/* const random2 = ( min , max) => {
-    return Math.floor((Math.random() * (max-min)) +min);
-}
-
-
-console.log (random2 ( 17 , 22 )) */
-
-
-/* if( jugada = 1 || 2 ) {
-    if (jugada = 1) {
-        const random = ( min , max) => {
-            return Math.floor((Math.random() * (max-min)) +min);
-        }
-        resultado = random ( 1 , 11 )
-        console.log (resultado)
-    }
-}else {
-    alert ('Introduce el valor correcto')
-} */
 
 let num = parseInt(prompt('Ingrese el monto del prestamo','100000'));
-let cuotas = parseInt(prompt('Ingrese cantidad de cuotas','12'))
+let cuotas = parseInt(prompt('Ingrese cantidad de cuotas','36'))
 let mensaje;
 
 // calculadora de prestamos
@@ -93,12 +50,12 @@ const verificar = (numero) => {
             if (num < 10000) {
                 mensaje = alert ('El prestamo esta fuera de rango')
                 num = parseInt(prompt('Ingrese un monto entre 10.000 y 100.000','100000'))
-                cuotas = parseInt(prompt('Ingrese cantidad de cuotas','12'))
+                cuotas = parseInt(prompt('Ingrese cantidad de cuotas','36'))
             }else{
                 while (num > 100000) {
                     mensaje = alert ('El prestamo esta fuera de rango')
                     num = parseInt(prompt('Ingrese un monto entre 10.000 y 100.000','100000'))
-                    cuotas = parseInt(prompt('Ingrese cantidad de cuotas','12'))
+                    cuotas = parseInt(prompt('Ingrese cantidad de cuotas','36'))
                 }
                 verificar (num)
             }
@@ -107,7 +64,7 @@ const verificar = (numero) => {
     }else{
         mensaje = alert ('Debes ingresar solo numeros')
         num = parseInt(prompt('Ingrese el monto de prestamo','100000'))
-        cuotas = parseInt(prompt('Ingrese cantidad de cuotas','12'))
+        cuotas = parseInt(prompt('Ingrese cantidad de cuotas','36'))
         verificar (num)
     }
 }
